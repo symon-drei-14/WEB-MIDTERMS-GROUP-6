b28aecbdd81feeb7b08289e4445385f881122d0b
Symon Alcedo <symonandrei.alcedo@letran.edu.ph>
2025-03-27 22:10:20 +0800

    luma huhu

--- Web Midterms Grp6/obj/Debug/net9.0/Web Midterms Grp6.AssemblyInfo.cs
+++ Web Midterms Grp6/obj/Debug/net9.0/Web Midterms Grp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a94b0f078fc064411f40f97aa9530a8c355167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9588a09225addf71fb4b8d1a15819c4f8d36286")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
