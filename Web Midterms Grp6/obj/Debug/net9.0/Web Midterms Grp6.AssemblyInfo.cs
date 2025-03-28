9a729e9e4a94e2983dc5df1d8ca6a1a989e62b93
Symon Alcedo <symonandrei.alcedo@letran.edu.ph>
2025-03-28 18:06:38 +0800

    driver icon

--- Web Midterms Grp6/obj/Debug/net9.0/Web Midterms Grp6.AssemblyInfo.cs
+++ Web Midterms Grp6/obj/Debug/net9.0/Web Midterms Grp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b5925b69899cd4409107a81802f4506e44395a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05d9d556edeecd08b1a02e3c5e1f4773811e3e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web Midterms Grp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
